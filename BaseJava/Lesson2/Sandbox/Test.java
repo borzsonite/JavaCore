2d6b259a9344dc5e65cd2f6dcab50116f0469a1a
Sergey Rozhkov <borzsonite@gmail.com>
2020-11-03 07:02:26 +0300

    Notes add

--- BaseJava/Lesson2/Sandbox/Test.java
+++ BaseJava/Lesson2/Sandbox/Test.java
@@ -8,7 +8,7 @@ Integer i = 5;
         int i=1; // Line 2. We are creating primitive local variable at line 2, so it’s created and stored in the stack memory of main() method.
         Object obj = new Object(); // Line 3. Since we are creating an Object in the 3rd line, it’s created in heap memory and stack memory contains the reference for it.
         Test mem = new Test(); // Line 4. A similar process occurs when we create Memory object in the 4th line.
-        mem.foo(obj); // Line 5 Now when we call the foo() method in the 5th line, a block in the top of the stack is created to be used by the foo() method. Since Java is pass-by-value, a new reference to Object is created in the foo() stack block in the 6th line.
+        mem.foo(obj); // Line 5. Now when we call the foo() method in the 5th line, a block in the top of the stack is created to be used by the foo() method. Since Java is pass-by-value, a new reference to Object is created in the foo() stack block in the 6th line.
     } // Line 9. In line 9, main() method terminates and the stack memory created for main() method is destroyed. Also, the program ends at this line, hence Java Runtime frees all the memory and ends the execution of the program.
 
     private void foo(Object param) { // Line 6
---
+++ BaseJava/Lesson2/Sandbox/Test2.java
@@ -0,0 +1,8 @@
+package BaseJava.Lesson2.Sandbox;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("abm".getClass());
+        System.out.println(Integer.toBinaryString(16));
+    }
+}

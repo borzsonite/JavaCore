8f746f3c569e4d00284d2b49ac5784da79332141
Sergey Rozhkov <borzsonite@gmail.com>
2020-11-21 20:08:47 +0300

    Test0 add

---
+++ BaseJava/Lesson5/Sandbox/Test0.java
@@ -0,0 +1,31 @@
+package BaseJava.Lesson5.Sandbox;
+
+import java.awt.*;
+import java.lang.reflect.Field;
+
+public abstract class Test0 {
+    private static String myName = "Hi";
+    private String ParentName;
+    protected static final int STORAGE_LIMIT = 11;
+
+
+    String getName() {
+        return myName;
+    }
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        Child child = new Child();
+        Class clazz = child.getClass();
+        Field field = child.getClass().getSuperclass().getDeclaredField("myName");
+        field.setAccessible(true);
+        System.out.println(child.getName());
+        field.set(child, "Hello");
+        System.out.println(child.getName());
+
+    }
+}
+
+class Child extends Test0 {
+    public String myName;
+
+}
